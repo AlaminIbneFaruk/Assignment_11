429830a1d4d8e0e563e7c54381268b4c11ca233c
AlaminIbneFaruk <alaminibnefarukayon@gail.com>
2025-02-20 14:50:05 +0600

    fixing errorpage

--- src/Pages/ErrorPages/ErrorPage.jsx
+++ src/Pages/ErrorPages/ErrorPage.jsx
@@ -1,5 +1,7 @@
+import Lottie from "lottie-react";
 import { Helmet } from "react-helmet";
 import { Link } from "react-router-dom";
+import ErrorLottie from "../../assets/lottie/error.json";
 
 const ErrorPage = () => {
   return (
@@ -7,30 +9,27 @@ const ErrorPage = () => {
       <Helmet>
         <title>404 Page not Found | Artifacts Bazzar</title>
       </Helmet>
-      <div className="bg-gradient-to-r from-stone-900  to-amber-500 min-h-screen flex justify-center items-center">
-        <div
-          className="hero max-w-3xl mx-auto my-auto"
-          style={{
-            backgroundImage: "url(https://i.ibb.co/KXhZ3BB/page-Not-Found.jpg)",
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-        >
-          <div className="hero-overlay bg-opacity-60 bg-black max-w-[calc(100%-200px)] h-80 rounded-lg m-10"></div>
-          <div className="hero-content text-neutral-content text-center">
-            <div className="max-w-md">
-              <p className="mb-2 text-3xl text-white">
-                Oops! The page you are looking for could not be found.
-              </p>
-              <Link
-                to="/"
-                className="mb-20 btn bg-gradient-to-b from-emerald-500 via-emerald-400 to-white text-black border-none"
-              >
-                Go Back Home
-              </Link>
-            </div>
+      <div className="bg-gradient-to-r from-stone-900 to-amber-500 min-h-screen flex justify-center items-center relative">
+        <div className="max-w-3xl mx-auto my-auto text-center relative z-10">
+
+          <div className="max-w-60 mx-auto mb-4">
+            <Lottie animationData={ErrorLottie} loop={true} />
+          </div>
+          <div className="max-w-md">
+            <p className="mb-2 text-3xl text-white">
+              Oops! The page you are looking for could not be found.
+            </p>
+            <Link
+              to="/"
+              className="mb-20 btn bg-gradient-to-b from-emerald-500 via-emerald-400 to-white text-black border-none"
+            >
+              Go Back Home
+            </Link>
           </div>
         </div>
+        
+        <div className="absolute inset-0 bg-black opacity-50 z-0"></div>{" "}
+        
       </div>
     </>
   );
